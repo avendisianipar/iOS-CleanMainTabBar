c322dd2cb50577727db59073eb6d215640e5627f
Avendi <avendi@tebengan.id>
2020-01-30 13:35:56 +0700

    commit

---
+++ iOS-CleanMainTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-CleanMainTabBar
+//
+//  Created by Avendi Sianipar on 30/1/20.
+//  Copyright © 2020 Avendi Sianipar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
